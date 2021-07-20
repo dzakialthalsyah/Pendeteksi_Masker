864df880e0a6d7ef4dda485ae455bbb1acfc25c0
dzakialthalsyah <68770143+dzakialthalsyah@users.noreply.github.com>
2021-07-21 00:02:37 +0700

    Update deteksi_masker.py

--- deteksi_masker.py
+++ deteksi_masker.py
@@ -53,7 +53,7 @@ ap.add_argument("-f", "--face", type=str,
 	default="face_detector",
 	help="path to face detector model directory")
 ap.add_argument("-m", "--model", type=str,
-	default="pendeteksi_masker.model",
+	default="mask_detector.model",
 	help="path to trained face mask detector model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
 	help="minimum probability to filter weak detections")
